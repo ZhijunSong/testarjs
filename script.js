51bb0531a909af08b976be5aea5adacee966ea49
Zhijun Song <zhijunsong@sencity.city>
2020-12-16 01:51:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,19 +13,20 @@ window.onload = () => {
     //     alert("Hi there!");
     //   });
     const clickListener = function () {
+            alert('Here!');
 
-            // after click, we are adding a label with the name of the place
-            const label = document.createElement('span');
-            const container = document.createElement('div');
-            container.setAttribute('id', 'place-label');
-            label.innerText = 'Talk to the mask.';
-            container.appendChild(label);
-            document.body.appendChild(container);
-
-            setTimeout(() => {
-                // that will disappear after less than 2 seconds
-                container.parentElement.removeChild(container);
-            }, 1500);
+            // // after click, we are adding a label with the name of the place
+            // const label = document.createElement('span');
+            // const container = document.createElement('div');
+            // container.setAttribute('id', 'place-label');
+            // label.innerText = 'Talk to the mask.';
+            // container.appendChild(label);
+            // document.body.appendChild(container);
+            //
+            // setTimeout(() => {
+            //     // that will disappear after less than 2 seconds
+            //     container.parentElement.removeChild(container);
+            // }, 1500);
          }
      };
     icon.addEventListener('click', clickListener);
