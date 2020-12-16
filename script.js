5f54bcd8aa5993081e107671966433519ddd3d8e
Zhijun Song <zhijunsong@sencity.city>
2020-12-16 01:06:09 -0500

    scriptjs click event

--- script.js
+++ script.js
@@ -1,83 +1,30 @@
 window.onload = () => {
-    let method = 'dynamic';
+    const scene = document.querySelector('a-scene');
+    const clickListener = function (ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
 
-    // if you want to statically add places, de-comment following line
-    method = 'static';
+        const name = ev.target.getAttribute('name');
+        const el = ev.detail.intersection && ev.detail.intersection.object.el;
 
-    if (method === 'static') {
-        let places = staticLoadPlaces();
-        renderPlaces(places);
-    }
+        if (el && el === ev.target) {
+            // after click, we are adding a label with the name of the place
+            const label = document.createElement('span');
+            const container = document.createElement('div');
+            container.setAttribute('id', 'place-label');
+            label.innerText = "tap to start";
+            container.appendChild(label);
+            document.body.appendChild(container);
 
-    if (method !== 'static') {
+            setTimeout(() => {
+                // that will disappear after less than 2 seconds
+                container.parentElement.removeChild(container);
+            }, 1500);
+         }
+     };
+     icon.addEventListener('click', clickListener);
 
-        // first get current user location
-        return navigator.geolocation.getCurrentPosition(function (position) {
+     scene.appendChild(icon);
 
-            // than use it to load from remote APIs some places nearby
-            dynamicLoadPlaces(position.coords)
-                .then((places) => {
-                    renderPlaces(places);
-                })
-        },
-            (err) => console.error('Error in retrieving position', err),
-            {
-                enableHighAccuracy: true,
-                maximumAge: 0,
-                timeout: 27000,
-            }
-        );
-    }
-};
-
-function staticLoadPlaces() {
-    return [
-        {
-            name: "Your place name",
-            location: {
-                lat: 0, // add here latitude if using static data
-                lng: 0, // add here longitude if using static data
-            }
-        },
-        {
-            name: 'Another place name',
-            location: {
-                lat: 0,
-                lng: 0,
-            }
-        }
-    ];
-}
-
-// getting places from REST APIs
-function dynamicLoadPlaces(position) {
-    let params = {
-        radius: 300,    // search places not farther than this value (in meters)
-        clientId: 'ILAXZHLG1YGODF5TOET4ICFT3I1CFJN5ZR1CRJQHDDS1IDI2',   // add your credentials here
-        clientSecret: 'CU54JRCCZPWDQRRLC1BNT3FJSEJGDV1LOYCOHVOEWEK53FOG',   // add your credentials here
-        version: '20300101',    // foursquare versioning, required but unuseful for this demo
-    };
-
-    // CORS Proxy to avoid CORS problems
-    let corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-    // Foursquare API
-    let endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-        &ll=${position.latitude},${position.longitude}
-        &radius=${params.radius}
-        &client_id=${params.clientId}
-        &client_secret=${params.clientSecret}
-        &limit=15
-        &v=${params.version}`;
-    return fetch(endpoint)
-        .then((res) => {
-            return res.json()
-                .then((resp) => {
-                    return resp.response.venues;
-                })
-        })
-        .catch((err) => {
-            console.error('Error with places API', err);
-        })
 
 };
